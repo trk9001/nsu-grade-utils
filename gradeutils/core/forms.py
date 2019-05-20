824f45cbc61f26e8aa90342c621cdc18efb68fe0
trk <dev.trk.9001@gmail.com>
2019-05-21 05:29:31 +0600

    Customize field student in TrimesterCreateForm
    The field now uses the HiddenInput widget instead of the default
    Select widget with disabled=True.

--- gradeutils/core/forms.py
+++ gradeutils/core/forms.py
@@ -5,10 +5,13 @@ from . import models
 
 class TrimesterCreateForm(forms.ModelForm):
 
+    student = forms.ModelChoiceField(
+        queryset=models.Student.objects.all(),
+        widget=forms.HiddenInput,
+        to_field_name='slug',
+        required=True,
+    )
+
     class Meta:
         model = models.Trimester
         fields = ['student', 'code']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['student'].widget.attrs.update({'disabled': True})
